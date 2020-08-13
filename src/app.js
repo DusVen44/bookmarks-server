0daf52b7c94e69cbbce2cae9f8ea7cfacfcf9241
Dustin Venable <Venny44@yahoo.com>
2020-08-13 15:55:19 -0500

    added stuff

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const bookmarksRouter = require('./bookmarks/bookmarks-router');
-const logger = require('./logger');
-const validateBearerToken = require('./validate-bearer-token');
-const errorHandler = require ('./error-handler');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
-const app = express();
+const app = express()
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'dev';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
-app.use(validateBearerToken);
+app.use('/api/bookmarks', bookmarksRouter)
 
 app.get('/', (req, res) => {
-    res.send('Bookmarks Homepage!')
-});
+  res.send('Hello, world!')
+})
 
-app.use(bookmarksRouter);
-
-app.use(errorHandler);
+app.use(errorHandler)
 
 module.exports = app
